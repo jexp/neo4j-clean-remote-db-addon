b512eaacc1725b844956558458c2accc9f11d5ed
Michael Hunger <github@jexp.de>
2011-06-01 15:24:13 +0200

    updated to 1.4.M03

--- src/main/java/org/neo4j/server/extension/test/delete/LocalTestServer.java
+++ src/main/java/org/neo4j/server/extension/test/delete/LocalTestServer.java
@@ -1,17 +1,29 @@
 package org.neo4j.server.extension.test.delete;
 
+import org.apache.commons.configuration.Configuration;
+import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.kernel.AbstractGraphDatabase;
+import org.neo4j.kernel.EmbeddedGraphDatabase;
 import org.neo4j.server.AddressResolver;
+import org.neo4j.server.Bootstrapper;
 import org.neo4j.server.NeoServerWithEmbeddedWebServer;
+import org.neo4j.server.configuration.PropertyFileConfigurator;
 import org.neo4j.server.database.Database;
+import org.neo4j.server.database.GraphDatabaseFactory;
 import org.neo4j.server.modules.RESTApiModule;
+import org.neo4j.server.modules.ServerModule;
 import org.neo4j.server.modules.ThirdPartyJAXRSModule;
 import org.neo4j.server.startup.healthcheck.StartupHealthCheck;
+import org.neo4j.server.startup.healthcheck.StartupHealthCheckRule;
 import org.neo4j.server.web.Jetty6WebServer;
 
 import java.io.File;
 import java.net.URI;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author mh
@@ -24,7 +36,7 @@ public class LocalTestServer {
     protected String propertiesFile = "test-db.properties";
 
     public LocalTestServer() {
-        this("localhost",7473);
+        this("localhost", 7473);
     }
 
     public LocalTestServer(String hostname, int port) {
@@ -33,20 +45,39 @@ public class LocalTestServer {
     }
 
     public void start() {
-        if (neoServer!=null) throw new IllegalStateException("Server already running");
+        if (neoServer != null) throw new IllegalStateException("Server already running");
         URL url = getClass().getResource("/" + propertiesFile);
-        if (url==null) throw new IllegalArgumentException("Could not resolve properties file "+propertiesFile);
-        neoServer = new NeoServerWithEmbeddedWebServer(new AddressResolver() {
+        if (url == null) throw new IllegalArgumentException("Could not resolve properties file " + propertiesFile);
+        final List<Class<? extends ServerModule>> serverModules = Arrays.asList(RESTApiModule.class, ThirdPartyJAXRSModule.class);
+        final Bootstrapper bootstrapper = new Bootstrapper() {
+            @Override
+            protected GraphDatabaseFactory getGraphDatabaseFactory(Configuration configuration) {
+                return new GraphDatabaseFactory() {
+                    @Override
+                    public AbstractGraphDatabase createDatabase(String databaseStoreDirectory, Map<String, String> databaseProperties) {
+                        return new EmbeddedGraphDatabase(databaseStoreDirectory, databaseProperties);
+                    }
+                };
+            }
+
+            @Override
+            protected Iterable<StartupHealthCheckRule> getHealthCheckRules() {
+                return Collections.emptyList();
+            }
+
+            @Override
+            protected Iterable<Class<? extends ServerModule>> getServerModules() {
+                return serverModules;
+            }
+        };
+        final AddressResolver addressResolver = new AddressResolver() {
             @Override
             public String getHostname() {
                 return hostname;
             }
-        }, new StartupHealthCheck(), new File(url.getPath()), new Jetty6WebServer()) {
-            protected void registerServerModules() {
-                registerModule(RESTApiModule.class);
-                registerModule(ThirdPartyJAXRSModule.class);
-            }
-
+        };
+        neoServer = new NeoServerWithEmbeddedWebServer(bootstrapper
+                , addressResolver, new StartupHealthCheck(), new PropertyFileConfigurator(new File(url.getPath())), new Jetty6WebServer(), serverModules) {
             @Override
             protected int getWebServerPort() {
                 return port;
@@ -56,8 +87,12 @@ public class LocalTestServer {
     }
 
     public void stop() {
-        neoServer.stop();
-        neoServer=null;
+        try {
+            neoServer.stop();
+        } catch (Exception e) {
+            System.err.println("Error stopping server: " + e.getMessage());
+        }
+        neoServer = null;
     }
 
     public int getPort() {
@@ -72,6 +107,7 @@ public class LocalTestServer {
         this.propertiesFile = propertiesFile;
         return this;
     }
+
     public Database getDatabase() {
         return neoServer.getDatabase();
     }
@@ -81,11 +117,11 @@ public class LocalTestServer {
     }
 
     public void cleanDb() {
-        Neo4jDatabaseCleaner cleaner = new Neo4jDatabaseCleaner(getGraphDatabase());
-        cleaner.cleanDb(Long.MAX_VALUE);
+        Neo4jDatabaseCleaner cleaner = new Neo4jDatabaseCleaner(getDatabase().graph);
+        cleaner.cleanDb();
     }
 
-    public AbstractGraphDatabase getGraphDatabase() {
+    public GraphDatabaseService getGraphDatabase() {
         return getDatabase().graph;
     }
 }
